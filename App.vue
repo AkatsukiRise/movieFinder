17ee4a039cd247369480efeab7dc6a5a29f9792c
bobsterrr <lobanovdaniilll@gmail.com>
2025-08-13 13:40:36 +0200

    feat: project started

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div id='container'>
+    <h2>BobCinema</h2>
+      <input
+        id='search'
+        placeholder='Название Фильма'
+      >
+  </div>
+</template>
+
+<style scoped>
+#container {
+  max-width: 50rem;
+  margin: auto;
+}
+
+h2 {
+  font-size: 2.2rem;
+}
+</style>
