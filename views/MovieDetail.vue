fd1ddca7004b6d852ec1875e82d7bf665c10a1bd
bobsterrr <lobanovdaniilll@gmail.com>
2025-08-14 13:55:22 +0200

    feat: movie details added

--- views/MovieDetail.vue
+++ views/MovieDetail.vue
@@ -1,14 +1,50 @@
 <script setup>
 import {ref, onBeforeMount} from 'vue';
 import {useRoute} from 'vue-router';
+import env from '@/env.js'
 
+const movie = ref({});
+const route = useRoute();
+
+onBeforeMount(() => {
+  fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+    .then(response => response.json())
+    .then(data => {
+      movie.value = data;
+    })
+});
 </script>
 
 <template>
   <div class='movie_detail'>
-    <h2>Movie title</h2>
+    <h2>{{movie.Title}}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src='movie.Poster' alt='Poster' class='featured_img'>
+    <p>{{ movie.Plot }}</p>
   </div>
 </template>
 
 <style>
+.movie_detail {
+  padding: 1rem;
+}
+
+.movie_detail h2 {
+  color: var(--yellow);
+  font-size: 1.75rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+
+.movie_detail .featured_img {
+  display: block;
+  max-width: 12.5rem;
+  margin-bottom: 1rem;
+}
+
+.movie_detail p {
+  color: var(--yellow);
+  font-size: 1.2rem;
+  line-height: 1.4;
+}
 </style>
